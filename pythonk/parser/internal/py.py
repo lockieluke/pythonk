239eab2f53c4f73124fb806447e2c67acfe3e161
lockieluke <sherlockluk@outlook.com>
2021-02-24 15:19:00 +0800

    Added nocompile() function

--- pythonk/parser/internal/py.py
+++ pythonk/parser/internal/py.py
@@ -6,6 +6,7 @@ class Py(StandardGrammar):
 
     def load_grammar(self):
         @self.parser.production("program : PY_EVAL OPEN_PAREN expression CLOSE_PAREN SEMICOLON")
+        @self.parser.production("program : NO_COMPILE OPEN_PAREN expression CLOSE_PAREN SEMICOLON")
         def py_eval(p):
             return PyEvalOperation(p[2])
         pass
