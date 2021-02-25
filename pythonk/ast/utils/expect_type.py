e193edac9ef7d864b47cdcb534b9cacd44c1f407
lockieluke <sherlockluk@outlook.com>
2021-02-25 10:37:06 +0800

    Used new type checking library for math operation

--- pythonk/ast/utils/expect_type.py
+++ pythonk/ast/utils/expect_type.py
@@ -7,12 +7,16 @@ class ExpectType:
 
     @staticmethod
     def check_type(value: Token, expected_type: BaseType):
-        got_type = type(value).__name__
+        got_type = ExpectType.get_exp_type(value)
         expected_type = expected_type.__name__
 
         if got_type is not expected_type:
             raise ArgumentTypeError(f"{expected_type} cannot be assigned with type {got_type}")
         pass
+
+    @staticmethod
+    def get_exp_type(value: Token) -> str:
+        return type(value).__name__
 
     pass
 
